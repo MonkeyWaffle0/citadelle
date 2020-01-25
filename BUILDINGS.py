870d8f79eedf2a2ff0ab0611f8bcbf6fd0ab5c0b
Monkeywaffle0 <rea.florian@gmail.com>
2020-01-25 10:28:29 -0500

    Added the buildings but no merveille

---
+++ BUILDINGS.py
@@ -0,0 +1,23 @@
+from classes.Building import *
+
+PALAIS = Building("Yellow", "Palais", 5, 5)           # x3
+MANOIR = Building("Yellow", "Manoir", 3, 3)           # x5
+CHATEAU = Building("Yellow", "Chateau", 4, 4)         # x4
+
+
+TOUR_DE_GUET = Building("Red", "Tour de guet", 1, 1)  # x3
+FORTERESSE = Building("Red", "Forteresse", 5, 5)      # x2
+PRISON = Building("Red", "Prison", 2, 2)              # x3
+CASERNE = Building("Red", "Caserne", 3, 3)            # x3
+
+CATHEDRALE = Building("Blue", "Cathédrale", 5, 5)     # x2
+TEMPLE = Building("Blue", "Temple", 1, 1)             # x3
+MONASTERE = Building("Blue", "Monastère", 3, 3)       # x3
+EGLISE = Building("Blue", "Eglise", 2, 2)             # x3
+
+COMPTOIR = Building("Green", "Comptoir", 3, 3)        # x3
+TAVERNE = Building("Green", "Taverne", 1, 1)          # x4
+MARCHE = Building("Green", "Marché", 2, 2)            # x4
+PORT = Building("Green", "Port", 4, 4)                # x3
+HOTEL_DE_VILLE = Building("Green", "Hotel de ville", 5, 5)  # x2
+ECHOPPE = Building("Green", "Echoppe", 2, 2)          # x3
